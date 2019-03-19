a940fc877b4e72af391094b7eb7e608c37a5c0df
hamed hossani <hamed.hossani@gmail.com>
2019-03-19 00:15:02 -0700

    add signInSuccessUrl for unappear error
    1-signInSuccessUrl always required error showed.
    2- add this.props.handleSignOut() for signout

--- src/page/SignInPage.jsx
+++ src/page/SignInPage.jsx
@@ -7,24 +7,53 @@ class SignInPage extends Component {
     uiConfig: {
       // Popup signin flow rather than redirect flow.
       signInFlow: 'popup',
+      signInSuccessUrl :this.props.signInSuccessUrl,
       // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
       callbacks: {
-        signInSuccess: this.props.signInSuccess
+        //signInSuccess: this.props.signInSuccess
+         // Avoid redirects after sign-in.
+          signInSuccessWithAuthResult: () => false
       },
       // We will display Google and Facebook as auth providers.
       signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID]
     }
   }
-  render() {
-    return (
-      <div>
-        <StyledFirebaseAuth
-          uiConfig={this.state.uiConfig}
-          firebaseAuth={firebase.auth()}
-        />
-      </div>
-    )
+  // Listen to the Firebase Auth state and set the local state.
+  componentDidMount() {
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+        (user) => this.setState({isSignedIn: !!user})
+    );
+  }
+  render() {    
+      if (!this.state.isSignedIn) {
+        return (
+          <div>         
+            <StyledFirebaseAuth
+              uiConfig={this.state.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          </div>
+        )
+      }else{
+        this.props.signInSuccess();
+        return (
+          <div>
+            <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
+            <span style={{
+              padding:10,
+              border:'1px solid',
+              borderRadius:8,
+              backgroundColor:'#ccc',
+              cursor:'pointer'
+            }}
+            onClick={
+              () => {
+                this.props.handleSignOut()
+              }
+              }>Sign-out</span>
+          </div>
+        );
+          }
   }
 }
-
 export default SignInPage
