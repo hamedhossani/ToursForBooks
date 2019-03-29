9752fd4f89207060b0ccc7808e8d13c880af4481
hamed hossani <hamed.hossani@gmail.com>
2019-03-29 06:02:35 -0700

    some changes
    show sign in page in middle of screen if you are not login.
    + some format code

--- src/page/SignInPage.jsx
+++ src/page/SignInPage.jsx
@@ -7,12 +7,12 @@ class SignInPage extends Component {
     uiConfig: {
       // Popup signin flow rather than redirect flow.
       signInFlow: 'popup',
-      signInSuccessUrl :this.props.signInSuccessUrl,
+      signInSuccessUrl: this.props.signInSuccessUrl,
       // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
       callbacks: {
         //signInSuccess: this.props.signInSuccess
-         // Avoid redirects after sign-in.
-          signInSuccessWithAuthResult: () => false
+        // Avoid redirects after sign-in.
+        signInSuccessWithAuthResult: () => false
       },
       // We will display Google and Facebook as auth providers.
       signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID]
@@ -21,39 +21,43 @@ class SignInPage extends Component {
   // Listen to the Firebase Auth state and set the local state.
   componentDidMount() {
     this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
-        (user) => this.setState({isSignedIn: !!user})
+      (user) => this.setState({ isSignedIn: !!user },
+        () => {
+          if (this.state.isSignedIn === true) {
+            this.props.signInSuccess();
+          }
+        })
     );
   }
-  render() {    
-      if (!this.state.isSignedIn) {
-        return (
-          <div>         
-            <StyledFirebaseAuth
-              uiConfig={this.state.uiConfig}
-              firebaseAuth={firebase.auth()}
-            />
-          </div>
-        )
-      }else{
-        this.props.signInSuccess();
-        return (
-          <div>
-            <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
-            <span style={{
-              padding:10,
-              border:'1px solid',
-              borderRadius:8,
-              backgroundColor:'#ccc',
-              cursor:'pointer'
-            }}
+  render() {
+    if (!this.state.isSignedIn) {
+      return (
+        <div>
+          <StyledFirebaseAuth
+            uiConfig={this.state.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
+          <span style={{
+            padding: 10,
+            border: '1px solid',
+            borderRadius: 8,
+            backgroundColor: '#ccc',
+            cursor: 'pointer'
+          }}
             onClick={
               () => {
                 this.props.handleSignOut()
               }
-              }>Sign-out</span>
-          </div>
-        );
-          }
+            }>Sign-out</span>
+        </div>
+      );
+    }
   }
 }
 export default SignInPage
